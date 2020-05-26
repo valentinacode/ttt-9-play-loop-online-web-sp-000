6797939acaf9c712f8e7c35efcc6ac26ce674199
Valentina E <tiyahelfrih@gmail.com>
2020-05-26 21:46:14 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,8 +36,8 @@ def turn(board)
 end
 
 # Define your play method below
-def play(board)
- puts board ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+#def play(board)
+# puts board ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
   
-end 
+#end 
 
